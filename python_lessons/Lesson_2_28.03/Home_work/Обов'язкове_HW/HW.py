38a07d4e0e1fe29ed11facd0d113f6fb060b62d4
Misha <msirko111@gmail.com>
2026-03-28 18:38:13 +0200

    Update HW.py

--- python_lessons/Lesson_2_28.03/Home_work/Обов'язкове_HW/HW.py
+++ python_lessons/Lesson_2_28.03/Home_work/Обов'язкове_HW/HW.py
@@ -4,18 +4,19 @@ if username == "":
     print("Username cannot be empty")
     exit()
 
-password = int(input("Enter your password: "))
+password = int(input("Enter your password (number): "))
 
 if password < 1000:
     print("Weak password")
+    exit()
 
-login_username = input("Enter your name: ")
+login_username = input("Enter your name to login: ")
 
 if login_username == "":
     print("Username cannot be empty")
     exit()
 
-login_password = int(input("Enter your password: "))
+login_password = int(input("Enter your password to login: "))
 
 if login_username != username:
     print("User not found")
